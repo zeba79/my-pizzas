4b12cdf082c459ca47eeb6daec4d574c26278494
Pantomik <addr@domain.ext>
2021-05-13 20:31:25 +0200

    Pizza orderring and Tests

--- sources/pizza.py
+++ sources/pizza.py
@@ -1,31 +1,11 @@
 
 RECIPES = {
-    'margherita': {
-        'ingredients': {'mozzarella': 2, 'oregano': 4},
-        'price': 10,
-        'sauce': 'tomato'
-    },
     'marinara': {
         'ingredients': {'garlic': 2, 'basil': 3},
         'price': 9,
     },
-    'quattro fromagi': {
-        'ingredients': {'mozzarella': 2, 'parmesan': 2, 'gorgonzola': 2, 'cheddar': 2, 'oregano': 3},
-        'price': 12,
-        'sauce': 'tomato'
-    },
-    'crudo': {
-        'ingredients': {'mozzarella': 2, 'ham': 3, 'basil': 3, 'mushroom': 3},
-        'price': 11,
-        'sauce': 'tomato'
-    },
-    'pugliese': {
-        'ingredients': {'mozzarella': 4, 'oregano': 2, 'onion': 5, 'mushroom': 2},
-        'price': 11,
-        'sauce': 'tomato'
-    },
     'romana': {
-        'ingredients': {'ham': 4, 'olive': 3, 'onion': 2, 'basil': 2},
+        'ingredients': {'ham': 4, 'olive': 5, 'onion': 2, 'basil': 2},
         'price': 12,
     },
     'original': {
@@ -42,8 +22,31 @@ RECIPES = {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
     },
+
     'my pizza' :{
     'ingredients' :{'gorgonzola':4, 'onion':2, 'parmesan':2, 'olive': 3},
     'price': 10,
     },
+
+    'margherita': {
+        'ingredients': {'mozzarella': 2, 'oregano': 4},
+        'price': 10,
+        'sauce': 'tomato'
+    },
+    'quattro fromagi': {
+        'ingredients': {'mozzarella': 2, 'parmesan': 2, 'gorgonzola': 2, 'cheddar': 2, 'oregano': 3},
+        'price': 12,
+        'sauce': 'tomato'
+    },
+    'crudo': {
+        'ingredients': {'mozzarella': 2, 'ham': 3, 'basil': 3, 'mushroom': 3},
+        'price': 11,
+        'sauce': 'tomato'
+    },
+    'pugliese': {
+        'ingredients': {'mozzarella': 4, 'oregano': 2, 'onion': 5, 'mushroom': 2},
+        'price': 11,
+        'sauce': 'tomato'
+    },
 }
+
