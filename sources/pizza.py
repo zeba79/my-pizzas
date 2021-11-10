2c3feca9de5d0bd7170c25dfeea06a374cbadf68
Antoine Zébazé <zeba_antoine@yahoo.fr>
2021-11-10 11:10:37 +0100

    Modification on sources/pizzeria.py file

--- sources/pizza.py
+++ sources/pizza.py
@@ -37,5 +37,5 @@ RECIPES = {
         'price': 11,
     },
     'my pizza' :{
-    'ingredients' :{ 'gorgonzola':4, 'onion':2, 'paremsan':2}
+    'ingredients' :{'gorgonzola':4, 'onion':2, 'paremsan':2}
 }
